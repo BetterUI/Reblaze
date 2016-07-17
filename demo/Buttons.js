7c926ebd0ab53cd2b70edac1f690d1c7b5bcb1a6
Rui Cheng <ktei2008@gmail.com>
2016-07-17 20:06:15 +1000

    Buttons

---
+++ demo/Buttons.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Button from 'lib/Button';
+import ButtonGroup from 'lib/ButtonGroup';
+
+const ButtonsDemo = () => (
+  <div>
+    <Button>Button</Button>
+    <Button href="#">Link</Button>
+    <Button primary>Button</Button>
+    <Button secondary>Button</Button>
+    <Button success>Button</Button>
+    <Button error>Button</Button>
+    <Button ghost primary>Button</Button>
+    <Button ghost secondary>Button</Button>
+    <Button ghost success>Button</Button>
+    <Button ghost error>Button</Button>
+    <Button size="xsmall">Button</Button>
+    <Button size="small">Button</Button>
+    <Button size="medium">Button</Button>
+    <Button size="large">Button</Button>
+    <Button size="super">Button</Button>
+    <Button rounded block>Button</Button>
+    <ButtonGroup>
+      <Button primary>Button</Button>
+      <Button secondary>Button</Button>
+    </ButtonGroup>
+    <ButtonGroup rounded>
+      <Button primary rounded>Button</Button>
+      <Button secondary rounded>Button</Button>
+    </ButtonGroup>
+  </div>
+);
+
+export default ButtonsDemo;
--- demo/index.js
+++ demo/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
 import Layout from 'demo/Layout';
 import InputsDemo from 'demo/Inputs';
+import ButtonsDemo from 'demo/Buttons';
 
 ReactDOM.render((
   <Router history={browserHistory}>
     <Route path="/" component={Layout}>
-      <Route path="input" component={InputsDemo} />
+      <Route path="inputs" component={InputsDemo} />
+      <Route path="buttons" component={ButtonsDemo} />
     </Route>
   </Router>
 ), document.getElementById('app'));
