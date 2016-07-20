b0b08838083fb0b83dbc36f37fb010181f179f16
Haotian Chang <cht8687@gmail.com>
2016-07-21 00:03:22 +1000

    Move routes to separate files.

--- demo/index.js
+++ demo/index.js
@@ -1,22 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory } from 'react-router';
-import Layout from 'demo/Layout';
-import AlertsDemo from 'demo/Alerts';
-import BubblesDemo from 'demo/Bubbles';
-import ButtonsDemo from 'demo/Buttons';
-import CardsDemo from 'demo/Cards';
-import InputsDemo from 'demo/Inputs';
-
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
 
 ReactDOM.render((
   <Router history={browserHistory}>
-    <Route path="/" component={Layout}>
-      <Route path="alerts" component={AlertsDemo} />
-      <Route path="bubbles" component={BubblesDemo} />
-      <Route path="buttons" component={ButtonsDemo} />
-      <Route path="cards" component={CardsDemo} />
-      <Route path="inputs" component={InputsDemo} />
-    </Route>
+    {routes}
   </Router>
 ), document.getElementById('app'));
---
+++ demo/routes.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Route } from 'react-router';
+import Layout from 'demo/Layout';
+import AlertsDemo from 'demo/Alerts';
+import BubblesDemo from 'demo/Bubbles';
+import ButtonsDemo from 'demo/Buttons';
+import CardsDemo from 'demo/Cards';
+import InputsDemo from 'demo/Inputs';
+
+export default (
+  <Route path="/" component={Layout}>
+    <Route path="alerts" component={AlertsDemo} />
+    <Route path="bubbles" component={BubblesDemo} />
+    <Route path="buttons" component={ButtonsDemo} />
+    <Route path="cards" component={CardsDemo} />
+    <Route path="inputs" component={InputsDemo} />
+  </Route>
+);
