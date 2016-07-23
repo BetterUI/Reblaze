ba33578d323918dfdbd16423acb6b9767cc00c92
Robert Chang <cht8687@gmail.com>
2016-07-23 19:37:51 +1000

    add quick link to new demos

--- demo/Home.js
+++ demo/Home.js
@@ -5,11 +5,14 @@ import { Link } from 'react-router';
 const Home = () => (
   <div>
     <Button primary><Link to="/alerts">Alerts</Link></Button>
-    <Button secondary><Link to="/bubbles">Bubbles</Link></Button>
-    <Button success><Link to="/buttons">Buttons</Link></Button>
-    <Button error><Link to="/cards">Cards</Link></Button>
-    <Button primary><Link to="/alignment">Alignment</Link></Button>
-    <Button secondary><Link to="/trees">Trees</Link></Button>
+    <Button secondary><Link to="/alignment">Alignment</Link></Button>
+    <Button success><Link to="/bubbles">Bubbles</Link></Button>
+    <Button error><Link to="/buttons">Buttons</Link></Button>
+    <Button primary><Link to="/cards">Cards</Link></Button>
+    <Button secondary><Link to="/drawers">Drawers</Link></Button>
+    <Button success><Link to="/headings">Headings</Link></Button>
+    <Button error><Link to="/menus">Menus</Link></Button>
+    <Button primary><Link to="/trees">Trees</Link></Button>
   </div>
 );
 
